cd8ee436debf2f7ec720a2d8c53e0199edf371d6
semanticdata <contact@miguelpimentel.do>
2024-04-29 14:49:56 -0500

    add comment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import {defineConfig} from 'vite'
 import {svelte} from '@sveltejs/vite-plugin-svelte'
 import {VitePWA} from 'vite-plugin-pwa'
-// import open, { apps } from 'open';
+import open, { apps } from 'open';
 
 export default defineConfig({
   plugins: [svelte(), VitePWA({registerType: 'autoUpdate'})],
-  // base: '/svelte-milkdown/',
+  // base: '/svelte-milkdown/', // only used for GitHub Pages
   publicDir: 'public',
   server: {
     open: '/index.html'
